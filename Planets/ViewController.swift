593eb6bbcca5e9a66621a166d169c3fb7491d3b7
chaitanya <chaitanya.kalavapudi@accenture.com>
2023-06-07 22:08:36 +0530

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by SravsRamesh on 07/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
